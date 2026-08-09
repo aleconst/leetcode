775ec699038b84a0ef16ba8c3eb0c438302126fa
Costy <147505153+aleconst@users.noreply.github.com>
2026-08-09 03:23:53 +0300

    Time: 5 ms (14.3%), Space: 14.2 MB (98.91%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        string lastPlace = paths[0][1];
+
+        for (int i = 1; i < paths.size(); i++)
+            for (int j = 0; j < paths.size(); j++)
+                if (lastPlace == paths[j][0])
+                    lastPlace = paths[i][1];
+
+        return lastPlace;
+    }
+};
