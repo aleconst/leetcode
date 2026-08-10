699af81e299dd99a77181aa29dddd54ff06f8b13
Costy <147505153+aleconst@users.noreply.github.com>
2026-08-10 21:32:16 +0300

    Time: 0 ms (100%), Space: 22.7 MB (18.97%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,13 +4,11 @@ public:
         int k = 1;
 
         for (int i = 1; i < nums.size(); i++)
-        {
             if (nums[i - 1] != nums[i])
             {
                 nums[k] = nums[i];
                 k++;
             }
-        }
 
         return k;
     }
