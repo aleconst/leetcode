dffb92b7212a1ba91177dba41a70f570af84276c
Costy <147505153+aleconst@users.noreply.github.com>
2026-08-11 21:13:31 +0300

    Time: 8 ms (17.59%), Space: 13.6 MB (20.02%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> result;
+        vector<int> ans;
 
         for (int i = 0; i < n; i++)
         {
-            result.push_back(nums[i]);
-            result.push_back(nums[i + n]);
+            ans.push_back (nums[i]);
+            ans.push_back (nums[i + n]);
         }
 
-        return result;
+        return ans;
     }
 };
