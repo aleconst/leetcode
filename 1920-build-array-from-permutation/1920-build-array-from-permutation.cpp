7461e19172d4f5976ad9a198f8ddc1350b926659
Costy <147505153+aleconst@users.noreply.github.com>
2026-08-11 21:10:41 +0300

    Time: 0 ms (100%), Space: 21 MB (5.15%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> ans;
 
         for (int i = 0; i < nums.size(); i++)
-            ans.push_back(nums[nums[i]]);
+            ans.push_back (nums[nums[i]]);
 
         return ans;
     }
