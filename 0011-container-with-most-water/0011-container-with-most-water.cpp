95b8f146dc9ce6db52bcc29a1afc1418271471b0
Costy <147505153+aleconst@users.noreply.github.com>
2026-08-12 23:22:39 +0300

    Time: 4 ms (20.53%), Space: 63 MB (49.7%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -4,19 +4,17 @@ public:
         int left = 0;
         int right = height.size() - 1;
         int amount = 0;
-        int max_amount = 0;
 
-        while (left < right)
+        while (left <= right)
         {
-            amount = (right - left) * min (height[left], height[right]);
-            max_amount = max  (max_amount, amount);
-
+            amount = max (amount, (right - left) * min (height[left], height[right]));
+            
             if (height[left] < height[right])
                 left++;
             else
                 right--;
         }
 
-        return max_amount;
+        return amount;
     }
 };
