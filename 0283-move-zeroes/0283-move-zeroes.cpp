21febd10ff948c7abe3bee15c000f39e71ef722a
Costy <147505153+aleconst@users.noreply.github.com>
2026-08-12 22:09:58 +0300

    Time: 0 ms (100%), Space: 23.8 MB (83.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int zero_counter = 0;
-        int k = 0;
+        int index = 0;
 
         for (int i = 0; i < nums.size(); i++)
         {
             if (nums[i])
             {
-                nums[k] = nums[i];
-                k++;
+                nums[index] = nums[i];
+                index++;
             }
             else
                 zero_counter++;
         }
 
-        k = nums.size() -1;
-        
-        while (zero_counter)
-        {
-            nums[k] = 0;
-            zero_counter--;
-            k--;
-        }
+        for (int i = 0; i < zero_counter; i++)
+            nums[nums.size() - i - 1] = 0;
     }
 };
