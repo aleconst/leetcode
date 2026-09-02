d30a98d3afac0564ea350874d801aeedcd692194
Ale <147505153+aleconst@users.noreply.github.com>
2026-09-02 03:04:16 +0300

    Time: 0 ms (100%), Space: 63 MB (11.2%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -3,18 +3,21 @@ public:
     int maxArea(vector<int>& height) {
         int left = 0;
         int right = height.size() - 1;
-        int amount = 0;
+        int maximum = -1;
 
-        while (left <= right)
+        while (left < right)
         {
-            amount = max (amount, (right - left) * min (height[left], height[right]));
-            
+            int amount = min (height[left], height[right]) * (right - left);
+
+            if (maximum < amount)
+                maximum = amount;
+
             if (height[left] < height[right])
                 left++;
             else
                 right--;
         }
 
-        return amount;
+        return maximum;
     }
 };
