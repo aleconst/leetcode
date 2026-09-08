ea1d2e584b90de63038024522ffdd1199c01e2ad
Ale <147505153+aleconst@users.noreply.github.com>
2026-09-08 22:01:55 +0300

    Time: 0 ms (100%), Space: 12.9 MB (32.18%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int> stk;
+
+        for (const string& c : operations)
+        {
+            if (c == "+")
+            {
+                int last = stk.top();
+                
+                stk.pop();
+
+                int sum = last + stk.top();
+                
+                stk.push(last);
+                stk.push(sum);
+            }
+            else if (c == "D")
+                stk.push(2 * stk.top());
+            else if (c == "C")
+                stk.pop();
+            else
+                stk.push(stoi(c));
+        }
+
+        int sum = 0;
+
+        while (!stk.empty())
+        {
+            sum += stk.top();
+            stk.pop();
+        }
+
+        return sum;
+    }
+};
